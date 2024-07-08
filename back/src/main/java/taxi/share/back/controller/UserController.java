c79bf703bb89305b5205ce91130584b204e44a54
dyd0027 <dyd002700@gmail.com>
2024-07-08 22:37:38 +0900

    회원가입 백 진행중

---
+++ back/src/main/java/taxi/share/back/controller/UserController.java
@@ -0,0 +1,24 @@
+package taxi.share.back.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import taxi.share.back.model.User;
+import taxi.share.back.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User registeredUser = userService.registerUser(user);
+        return ResponseEntity.ok(registeredUser);
+    }
+}
