57db87b3c32cff2a82a75edf68672ed5e633f625
dyd0027 <dyd002700@gmail.com>
2024-07-22 22:17:40 +0900

    fetch -> axios

--- front/src/api/user.ts
+++ front/src/api/user.ts
@@ -1,24 +1,20 @@
 // src/api/user.ts
+import axios from 'axios';
+
 export interface FormData {
-    userId: string;
-    userPassword: string;
-    userName: string;
-    phoneNum: string;
-    userSex: 'M' | 'F';
+  userId: string;
+  userPassword: string;
+  userName: string;
+  phoneNum: string;
+  userSex: 'M' | 'F';
 }
 
+// axios같은 경우 fetch보다 좀 더 유연하게 백엔드와의 통신을 할 수 있어서 axios를 많이 사용 함.
 export const user = async (formData: FormData): Promise<FormData> => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/users/register`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-    });
-
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-
-    return response.json();
+  const response = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/users/register`, formData, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return response.data;
 };
