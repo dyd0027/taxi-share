59204f722ceae4cd25f5ecfcbac4435428e2883f
dyd0027 <dyd002700@gmail.com>
2024-07-25 22:44:50 +0900

    로그 삽입

--- back/src/main/java/taxi/share/back/controller/UserController.java
+++ back/src/main/java/taxi/share/back/controller/UserController.java
@@ -1,11 +1,13 @@
 package taxi.share.back.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import taxi.share.back.model.User;
 import taxi.share.back.service.UserService;
 
+@Slf4j // log
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -19,9 +21,11 @@ public class UserController {
         return ResponseEntity.ok(registeredUser);
     }
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestParam String userId, @RequestParam String password) {
+    public ResponseEntity<?> login(@RequestParam String userId, @RequestParam String userPassword) {
+        log.info(userId);
         try {
-            String token = userService.login(userId, password);
+            String token = userService.login(userId, userPassword);
+            log.info(token);
             return ResponseEntity.ok(token);
         } catch (Exception e) {
             return ResponseEntity.status(401).body("Unauthorized: " + e.getMessage());
