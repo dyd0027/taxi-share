1ba347508f5ac05c417a1241efd7fe98fd61c22f
dyd0027 <dyd002700@gmail.com>
2024-08-12 22:19:13 +0900

    Redis 사용

--- back/src/main/java/taxi/share/back/service/UserService.java
+++ back/src/main/java/taxi/share/back/service/UserService.java
@@ -1,5 +1,8 @@
 package taxi.share.back.service;
 
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -9,7 +12,8 @@ import taxi.share.back.util.JwtUtil;
 
 @Service
 public class UserService {
-
+    @Autowired
+    private CacheManager cacheManager;
     @Autowired
     private UserRepository userRepository;
 
@@ -31,7 +35,17 @@ public class UserService {
         }
     }
 
+    @Cacheable(value = "userCache", key = "#userId")
     public User findUserByUserId(String userId) throws Exception {
+        // Redis에서 데이터 가져오는지 확인 하는 테스트 코드
+        // 실제로 Redis cache에 저장되면 findUserByUserId메소드를 호출하지 않음
+        // 작동 방식: 메서드가 호출될 때마다 캐시를 먼저 확인 -> 캐시가 있으면 캐시된 데이터를 반환 -> 캐시에 없으면 실제 메서드를 실행한 결과를 캐시에 저장하고 반환
+        Cache cache = cacheManager.getCache("userCache");
+        if (cache != null && cache.get(userId) != null) {
+            System.out.println("Cache hit for key: " + userId);
+        } else {
+            System.out.println("Cache miss for key: " + userId);
+        }
         return userRepository.findByUserId(userId)
                 .orElseThrow(() -> new Exception("User not found"));
     }
