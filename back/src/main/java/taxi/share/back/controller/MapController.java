8951a8e2aad7cc3d63291d52fb311d25c5eeb0d1
Parkcharito <qsqs128@naver.com>
2024-08-27 22:53:15 +0900

    백단 지도 모델,컨트롤러추가

---
+++ back/src/main/java/taxi/share/back/controller/MapController.java
@@ -0,0 +1,59 @@
+package taxi.share.back.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import taxi.share.back.model.User;
+import taxi.share.back.service.UserService;
+import taxi.share.back.util.JwtUtil;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public UserController(UserService userService, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User registeredUser = userService.registerUser(user);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user, HttpServletResponse response) {
+        log.info("Login attempt for user: {}", user.getUserId());
+        try {
+            User dbUser = userService.findUserByUserId(user.getUserId());
+            User valiedUser = userService.login(dbUser, user.getUserPassword(), response);
+            return ResponseEntity.ok(valiedUser);
+        } catch (Exception e) {
+            log.error("Login error: {}", e.getMessage());
+            return ResponseEntity.status(401).body(null);
+        }
+    }
+    @GetMapping("/check-session")
+    public ResponseEntity<Boolean> sessionCheck(HttpServletRequest request, HttpServletResponse response,@RequestParam(name = "cookie", required = false) String cookie) {
+        String token = jwtUtil.extractTokenFromCookie(request, cookie);
+        if (token != null) {
+            boolean isValidated = userService.validateToken(token, response);
+            if (!isValidated) {
+                jwtUtil.invalidateCookie(response, "jwt-token");
+            }
+            return ResponseEntity.ok(isValidated);
+        } else {
+            return ResponseEntity.ok(false);
+        }
+    }
+}
---
+++ back/src/main/java/taxi/share/back/model/Routes.java
@@ -0,0 +1,52 @@
+package taxi.share.back.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "USER")
+@Setter
+@Getter
+@Schema(description = "Details about the user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "The unique ID of the user", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
+    private int userNo;
+
+    @Column(nullable = false, unique = true)
+    @Schema(description = "The user's ID", example = "user123")
+    private String userId;
+
+    @Column(nullable = false)
+    @Schema(description = "The user's password", example = "password123")
+    private String userPassword;
+
+    @Column(nullable = false)
+    @Schema(description = "The user's name", example = "John Doe")
+    private String userName;
+
+    @Column(nullable = false)
+    @Schema(description = "The user's type", example = "1")
+    private int userType;
+
+    @Column(nullable = false, unique = true)
+    @Schema(description = "The user's phone number", example = "010-1111-1111")
+    private String phoneNum;
+
+    // char가 아닌 String인 이유는 SpringBoot는 JSON 데이터를 객체로 변환할 때 char를 처리하기 힘듦.
+    @Column(nullable = false)
+    @Pattern(regexp = "[MF]", message = "Sex must be M or F")
+    @Schema(description = "The user's sex", example = "M")
+    private String userSex;
+
+
+    // getters and setters
+}
