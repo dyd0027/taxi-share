86195149c2983d412d8b542db68fc7d013a537c3
GwonYongHwi <dyd002700@gmail.com>
2024-08-30 08:13:59 +0900

    back 카카오api 송수신

--- back/src/main/java/taxi/share/back/service/MapService.java
+++ back/src/main/java/taxi/share/back/service/MapService.java
@@ -1,19 +1,52 @@
 package taxi.share.back.service;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import taxi.share.back.model.Routes;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class MapService {
     @Value("${kakao.rest.api.key}")
     private String kakaoApiKey;
 
+    private final RestTemplate restTemplate;
+
     public void route(Routes routes){
-        log.info("출발지 >>> {}",routes.getOrigin());
-        log.info("kakaoApiKey >>> {}",kakaoApiKey);
+        String url = "https://apis-navi.kakaomobility.com/v1/directions";
+
+        // 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "KakaoAK " + kakaoApiKey);
+        headers.set("Content-Type", "application/json");
+
+        // 쿼리 파라미터 설정
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("origin", routes.getOriginLongitude() + "," + routes.getOriginLatitude())
+                .queryParam("destination", routes.getDestinationLongitude() + "," + routes.getDestinationLatitude());
+
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+        // API 호출
+        ResponseEntity<String> response = restTemplate.exchange(
+                uriBuilder.toUriString(),
+                HttpMethod.GET,
+                entity,
+                String.class
+        );
+
+        log.info("Kakao API Response: {}", response.getBody());
 
     }
 }
