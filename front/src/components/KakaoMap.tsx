5c25224e7e98397acd25e38fe55a8d5072b3b395
GwonYongHwi <dyd002700@gmail.com>
2024-08-30 08:13:23 +0900

    front 위도, 경도 수정

--- front/src/components/KakaoMap.tsx
+++ front/src/components/KakaoMap.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import { RouteData } from "@/types/routeData";
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 
 declare global {
   interface Window {
@@ -16,17 +14,19 @@ interface KakaoMapProps {
 }
 
 const KakaoMap = ({ origin, destination, setSendData }: KakaoMapProps) => {
-  const [latitude,setLatitude] = useState<number | 12>();
-  const [longitude,setLongitude] = useState<number | 0>();
+
+  const prevOriginRef = useRef<string | undefined>();
+  const prevDestinationRef = useRef<string | undefined>();
+
   useEffect(() => {
-    const kakaoMapScript = document.createElement('script');
+    const kakaoMapScript = document.createElement("script");
     kakaoMapScript.async = true;
     kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services&autoload=false`;
     document.head.appendChild(kakaoMapScript);
 
     kakaoMapScript.onload = () => {
       window.kakao.maps.load(() => {
-        const container = document.getElementById('map');
+        const container = document.getElementById("map");
         const options = {
           center: new window.kakao.maps.LatLng(37.5665, 126.9780), // 초기 중심 좌표를 서울로 설정
           level: 3,
@@ -34,48 +34,48 @@ const KakaoMap = ({ origin, destination, setSendData }: KakaoMapProps) => {
 
         const map = new window.kakao.maps.Map(container, options);
 
-        // services 객체가 존재하는지 확인 후 Geocoder 사용
         if (window.kakao.maps.services) {
           const geocoder = new window.kakao.maps.services.Geocoder();
-
-          // 주소를 좌표로 변환하고 마커를 생성하는 함수
           const setMarker = (address: string, title: string) => {
             geocoder.addressSearch(address, (result: any, status: any) => {
               if (status === window.kakao.maps.services.Status.OK) {
                 const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
-                setLatitude(Number(result[0].y));
-                setLongitude(Number(result[0].x));
+                if(title === "출발지"){
+                  setSendData((prevState) => ({
+                    ...prevState,
+                    origin: origin || "",
+                    originLatitude: Number(result[0].y),
+                    originLongitude: Number(result[0].x),
+                  }));
+                }else{
+                  setSendData((prevState) => ({
+                    ...prevState,
+                    destination: destination || "",
+                    destinationLatitude: Number(result[0].y),
+                    destinationLongitude: Number(result[0].x),
+                  }));
+                }
                 const marker = new window.kakao.maps.Marker({
                   map: map,
                   position: coords,
-                  title: title, // 마커에 제목을 설정
+                  title: title,
                 });
-
-                // 지도 중심을 마커 위치로 이동
                 map.setCenter(coords);
-
               }
             });
           };
-          // origin과 destination에 대해 마커를 설정
-          if (origin) {
-            setMarker(origin, "출발지");
-            setSendData((prevState) => ({
-                ...prevState,
-                origin: origin,
-                originLatitude: Number(latitude),
-                originLongitude: Number(longitude),
-            }));
+
+          if (origin !== prevOriginRef.current) {
+            setMarker(origin || "", "출발지");
           }
-          if (destination) {
-            setMarker(destination, "도착지");
-            setSendData((prevState) => ({
-              ...prevState,
-              destination: destination,
-              destinationLatitude: Number(latitude),
-              destinationLongitude: Number(longitude),
-          }));
+
+          if (destination !== prevDestinationRef.current) {
+            setMarker(destination || "", "도착지");
           }
+
+          // 이전 값을 업데이트
+          prevOriginRef.current = origin;
+          prevDestinationRef.current = destination;
         } else {
           console.error("Kakao Map services are not available.");
         }
@@ -88,7 +88,7 @@ const KakaoMap = ({ origin, destination, setSendData }: KakaoMapProps) => {
         document.head.removeChild(kakaoMapScript);
       }
     };
-  }, [origin, destination]); // origin과 destination이 변경될 때마다 useEffect 실행
+  }, [origin, destination]);
 
   return <div id="map" style={{ width: "100%", height: "500px" }}></div>;
 };
