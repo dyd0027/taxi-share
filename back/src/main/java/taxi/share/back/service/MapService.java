6d4850c771456ac4a43c8258a5c959c4aba6e686
GwonYongHwi <dyd002700@gmail.com>
2024-09-24 17:00:36 +0900

    경로 join 개발중

--- back/src/main/java/taxi/share/back/service/MapService.java
+++ back/src/main/java/taxi/share/back/service/MapService.java
@@ -25,13 +25,13 @@ import taxi.share.back.repository.RoutesRepository;
 public class MapService {
     @Value("${kakao.rest.api.key}")
     private String kakaoApiKey;
-
+    @Value("${kakao.rest.api.url}")
+    private String kakaoApiURL;
     private final RestTemplate restTemplate;
     private final RoutesRepository routesRepository;
     private final CacheManager cacheManager;
     private final RedisService redisService;
     public String route(Routes routes){
-        String url = "https://apis-navi.kakaomobility.com/v1/directions";
 
         // 헤더 설정
         HttpHeaders headers = new HttpHeaders();
@@ -39,7 +39,7 @@ public class MapService {
         headers.set("Content-Type", "application/json");
 
         // 쿼리 파라미터 설정
-        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url)
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(kakaoApiURL)
                 .queryParam("origin", routes.getOriginLongitude() + "," + routes.getOriginLatitude())
                 .queryParam("destination", routes.getDestinationLongitude() + "," + routes.getDestinationLatitude());
 
@@ -124,51 +124,64 @@ public class MapService {
         return true;
     }
 
-    // test용
-    public String routeWithWaypoints(Routes route1, Routes route2) {
-        String url = "https://apis-navi.kakaomobility.com/v1/directions";
-
+    public String routeJoin(Routes route) {
         // 헤더 설정
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "KakaoAK " + kakaoApiKey);
         headers.set("Content-Type", "application/json");
 
         // 출발지, 경유지 및 목적지 설정 (하드코딩)
-        String origin = route1.getOriginLongitude() + "," + route1.getOriginLatitude();
-        String waypoint1 = route2.getOriginLongitude() + "," + route2.getOriginLatitude(); // 두 번째 사용자의 출발지
-        String waypoint2 = route2.getDestinationLongitude() + "," + route2.getDestinationLatitude(); //  두 번째 사용자의 목적지
-        String destination = route1.getOriginLongitude() + "," + route1.getOriginLatitude(); // 첫 번째 사용자의 목적지
+        String origin = route.getOriginLongitude() + "," + route.getOriginLatitude();
+        String destination = route.getOriginLongitude() + "," + route.getOriginLatitude(); // 첫 번째 사용자의 목적지
+        findNearRoute(route);
 
-        // 경유지 문자열 생성
-        // String waypoints = waypoint1 + "|" + waypoint2;
-
-
-        // 경유지 문자열 생성 (단일 경유지로 테스트)
-        String waypoints = waypoint1;
-        String waypoints2 = waypoint2;
-
-        // 쿼리 파라미터 설정
-        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url)
-                .queryParam("origin", origin)
-                .queryParam("destination", destination)
-                .queryParam("waypoints", waypoints)
-                .queryParam("waypoints", waypoints2);
-
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
-
-        // API 호출
-        ResponseEntity<String> response = restTemplate.exchange(
-                uriBuilder.toUriString(),
-                HttpMethod.GET,
-                entity,
-                String.class
-        );
-
-        log.info("Kakao API Response: {}", response.getBody());
-
-        return response.getBody();
+        return "return";
     }
 
+    public void findNearRoute(Routes route){
 
+    }
+
+//    public String routeWithWaypoints(Routes route) {
+//        // 헤더 설정
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set("Authorization", "KakaoAK " + kakaoApiKey);
+//        headers.set("Content-Type", "application/json");
+//
+//        // 출발지, 경유지 및 목적지 설정 (하드코딩)
+//        String origin = route1.getOriginLongitude() + "," + route1.getOriginLatitude();
+//        String waypoint1 = route2.getOriginLongitude() + "," + route2.getOriginLatitude(); // 두 번째 사용자의 출발지
+//        String waypoint2 = route2.getDestinationLongitude() + "," + route2.getDestinationLatitude(); //  두 번째 사용자의 목적지
+//        String destination = route1.getOriginLongitude() + "," + route1.getOriginLatitude(); // 첫 번째 사용자의 목적지
+//
+//        // 경유지 문자열 생성
+//        // String waypoints = waypoint1 + "|" + waypoint2;
+//
+//
+//        // 경유지 문자열 생성 (단일 경유지로 테스트)
+//        String waypoints = waypoint1;
+//        String waypoints2 = waypoint2;
+//
+//        // 쿼리 파라미터 설정
+//        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(kakaoApiURL)
+//                .queryParam("origin", origin)
+//                .queryParam("destination", destination)
+//                .queryParam("waypoints", waypoints)
+//                .queryParam("waypoints", waypoints2);
+//
+//        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+//
+//        // API 호출
+//        ResponseEntity<String> response = restTemplate.exchange(
+//                uriBuilder.toUriString(),
+//                HttpMethod.GET,
+//                entity,
+//                String.class
+//        );
+//
+//        log.info("Kakao API Response: {}", response.getBody());
+//
+//        return response.getBody();
+//    }
 
 }
